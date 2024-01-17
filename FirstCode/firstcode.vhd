e5a59434230a6c40208ef2e9213051fdbcd9d2f6
rbrin <rbrinson2@gmail.com>
2024-01-17 17:54:33 -0500

    Switched to naming convention

--- FirstCode/firstcode.vhd
+++ FirstCode/firstcode.vhd
@@ -3,18 +3,18 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 
-entity firstcode is
+entity Intro is
     port (
         sw: in std_ulogic_vector(3 downto 1);
         led: out std_ulogic_vector(3 downto 1)
     );
-end entity firstcode;
+end entity Intro;
 
-architecture rtl of firstcode is
+architecture Intro_ARCH of Intro is
     
 begin
     led(1) <= sw(1) or sw(2) or sw(3);
     led(2) <= (sw(1) and sw(2)) or (sw(1) and sw(3)) or (sw(2) and sw(3));
     led(3) <= sw(1) and sw(2) and sw(3);
     
-end architecture rtl;
+end architecture Intro_ARCH;
