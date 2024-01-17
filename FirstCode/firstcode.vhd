cdc689799a70ffda265b3771e7ccd28cead16989
rbrin <rbrinson2@gmail.com>
2024-01-17 17:47:30 -0500

    Changed in/out to vectors

--- FirstCode/firstcode.vhd
+++ FirstCode/firstcode.vhd
@@ -5,21 +5,16 @@ use IEEE.numeric_std.all;
 
 entity firstcode is
     port (
-        sw1: in std_ulogic;
-        sw2: in std_ulogic;
-        sw3: in std_ulogic;
-        led1: out std_ulogic;
-        led2: out std_ulogic;
-        led3: out std_ulogic
+        sw: in std_ulogic_vector(3 downto 1);
+        led: out std_ulogic_vector(3 downto 1)
     );
 end entity firstcode;
 
 architecture rtl of firstcode is
     
 begin
-    
-    led1 <= sw1 or sw2 or sw3;
-    led2 <= (sw1 and sw2) or (sw1 and sw3) or (sw2 and sw3);
-    led3 <= sw1 and sw2 and sw3;
+    led(1) <= sw(1) or sw(2) or sw(3);
+    led(2) <= (sw(1) and sw(2)) or (sw(1) and sw(3)) or (sw(2) and sw(3));
+    led(3) <= sw(1) and sw(2) and sw(3);
     
 end architecture rtl;
