9a0b0b54e9287c0f3b94253a7999a647e855c26c
rbrinson2 <rbrinson2@gmail.com>
2024-02-15 14:24:27 -0500

    Added commenting

--- Lab2_SwitchToLED/Modelsim/JoyToLed.vhd
+++ Lab2_SwitchToLED/Modelsim/JoyToLed.vhd
@@ -4,24 +4,35 @@ library ieee;
 
 entity JoyToLed is
     port (
-        leftJoy     : std_logic;
-        rightJoy    : std_logic;
-        signal switches    : in std_logic_vector(2 downto 0);
-        signal outputLed   : out std_logic_vector(15 downto 0)
-        --sevenSegs   : std_logic_vector(6 downto 0)
+        ----- Input Ports -----
+        leftJoy             : in std_logic;
+        rightJoy            : in std_logic;
+        signal switches     : in std_logic_vector(2 downto 0);
+        signal outputLed    : out std_logic_vector(15 downto 0)
+
+        ----- Output Ports -----
+        --signal sevenSegs  : out std_logic_vector(6 downto 0)
     );
 end entity;
 
 architecture JoyToLED_ARCH of JoyToLed is
+    ----- Constants -----
+    constant ON: std_logic := '1';
+    constant OFF: std_logic := '0';
+    ----- Internal Signals -----
     signal leftLed  : std_logic_vector(7 downto 0);
     signal rightLed : std_logic_vector(7 downto 0);
 begin
 
-    outputLed(7 downto 0) <=  rightLed when (rightJoy = '1') else
+    -- Final Led output
+    outputLed(15 downto 8) <= leftLed when (leftJoy = ON) else
                                 (others => '0');
-    outputLed(15 downto 8) <= leftLed when (leftJoy = '1') else
+    outputLed(7 downto 0)  <= rightLed when (rightJoy = ON) else
                                 (others => '0');
+    
+    ----- Processes -----
 
+    -- Mirror to reflect right to left
     MIRROR: process (rightLed) is
     begin
         for i in leftLed'range loop
@@ -29,21 +40,25 @@ begin
         end loop;
     end process;
 
+    -- Led Driver to convert switch count to leds
     LED_DRIVER: process (switches) is
         variable count: integer range 0 to 7 := 0;
     begin
         count := 0;
+
+        -- For loop to count active switches
         for i in switches'range loop
-            if (switches(i) = '1') then
+            if (switches(i) = ON) then
                 count := count + (2 ** i);
             end if;
         end loop;
         
+        -- For loop to convert to leds active
         for i in rightLed'range loop
             if (i < count) then
-                rightLed(i) <= '1';
+                rightLed(i) <= ON;
             else
-                rightLed(i) <= '0';
+                rightLed(i) <= OFF;
             end if;
         end loop;
     end process LED_DRIVER;
