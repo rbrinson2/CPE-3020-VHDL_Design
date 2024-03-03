de6d4ad85f10a8d8c905555bac96dac8eb5a0cef
rbrinson2 <rbrinson2@gmail.com>
2024-03-03 18:59:26 -0500

    added second process to testbench

--- Lab3_Moving_LED/Modelsim/MovingLed_TB.vhd
+++ Lab3_Moving_LED/Modelsim/MovingLed_TB.vhd
@@ -8,13 +8,19 @@ end entity MovingLed_TB;
 
 architecture MovingLed_TB_ARCH of MovingLed_TB is
 
-    signal clock        : std_logic := '0';
-    signal reset        : std_logic := '1';
-    signal rightMove    : std_logic := '1';
-    signal leftMove     : std_logic := '0';
-    signal led          : std_logic_vector(15 downto 0);
-    signal sevenSegs    : std_logic_vector(6 downto 0);
-    signal anodes       : std_logic_vector(3 downto 0);
+    signal clock            : std_logic := '0';
+    signal reset            : std_logic := '1';
+    signal rightMove        : std_logic := '1';
+    signal leftMove         : std_logic := '0';
+    signal led              : std_logic_vector(15 downto 0);
+    signal sevenSegs        : std_logic_vector(6 downto 0);
+    signal anodes           : std_logic_vector(3 downto 0);
+
+    signal stimProcDone     : std_logic := '0';
+    signal stimRightMove    : std_logic := '0';
+    signal stimLeftMove     : std_logic := '0';
+    signal stim2RightMove    : std_logic := '0';
+    signal stim2LeftMove     : std_logic := '0';
   
 begin
 
@@ -32,22 +38,27 @@ begin
         anodes    => anodes
     );
     
+    with stimProcDone select
+        rightMove <=    stim2RightMove when '1',
+                        stimRightMove when '0',
+                        '0' when others;
+
     STIMULUS: process (clock) is
-        variable clockCount : integer := 0;
-        variable moveCount : integer := 0;
+        variable clockCount : integer range 0 to 1 := 0;
+        variable moveCount : integer range 0 to 31 := 0;
     begin
         if (clockCount mod 4 = 0) then
             if (moveCount < 16) then
                 leftMove <= '1';
             elsif (moveCount >= 16) then
-                rightMove <= '1';
+                stimRightMove <= '1';
             end if;
             moveCount := moveCount + 1;
         elsif (clockCount mod 4 = 2) then
             if (moveCount < 16) then
                 leftMove <= '0';
             elsif (moveCount >= 16) then   
-                rightMove <= '0';
+                stimRightMove <= '0';
             end if;
         end if;
         
@@ -55,10 +66,21 @@ begin
             leftMove <= '0';
         end if;
         if (moveCount < 16) then
-            rightMove <= '0';
+            stimRightMove <= '0';
         end if;
+
+        if (moveCount > 32) then
+            stimProcDone <= '1';
+        end if;
+
         clockCount := clockCount + 1;
     end process;
 
+    SECONDPROC: process (clock) is
+    begin
+        if (stimProcDone = '1') then
+            stim2RightMove <= '1';
+        end if;
+    end process;
 	
 end architecture MovingLed_TB_ARCH;
