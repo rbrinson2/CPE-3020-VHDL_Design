56b801934e3eac6b435c8a7e8b38b53a43e2ca90
rbrinson2 <rbrinson2@gmail.com>
2024-03-07 07:59:15 -0500

    Practicum 1

---
+++ Practicum1/Sources/Practicum1.vhd
@@ -0,0 +1,52 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+entity Practicum1 is
+    port (
+        ----- Inputs
+        switches    : in std_logic_vector (7 downto 0);
+
+        ----- Outputs
+        led         : out std_logic_vector (1 downto 0)
+    );
+end entity;
+
+architecture Practicum1_ARCH of Practicum1 is
+begin
+
+
+
+    ZERO_COUNT: process (switches) is
+        variable even       : std_logic;
+        variable odd        : std_logic;
+        variable zeroCount  : integer range 0 to 7 := 0;
+    begin
+        for i in switches'range loop
+            if (switches(i) = '0') then
+                zeroCount := zeroCount + 1;
+            end if;
+        end loop;
+        
+        if (zeroCount = 0) then
+            even    := '0';
+            odd     := '0';
+        elsif (zeroCount mod 2 = 0) then
+            even    := '1';
+            odd     := '0';
+        elsif (zeroCount mod 2 = 1) then
+            even    := '0';
+            odd     := '1';
+        end if;
+
+        zeroCount := 0;
+        led <= odd & even;
+    end process;
+
+end architecture;
---
+++ Practicum1/Sources/Practicum1_TB.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+entity Practicum1_TB is
+end entity;
+
+architecture Practicum1_TB_ARCH of Practicum1_TB is
+    signal clock    : std_logic := '0';
+    signal switches : std_logic_vector (7 downto 0) := (others => '0');
+    signal led      : std_logic_vector (1 downto 0) := (others => '0');
+begin
+
+    clock <= not clock after 10 ns;
+
+    practicum1_inst: entity work.Practicum1
+    port map (
+        switches => switches,
+        led      => led
+    );
+
+    STIMULUS: process (clock) is
+        variable count : integer := 0;
+    begin
+        if (rising_edge(clock)) then
+            switches <= std_logic_vector(to_unsigned(count, 8));
+            count := count + 1;
+        end if;
+        
+
+        if (count >= 260) then
+            std.env.stop;
+        end if;
+    end process;
+end architecture;
