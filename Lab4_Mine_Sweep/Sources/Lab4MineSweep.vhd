3ad033fb7d7107dfd5ffcda299d4ef167cd37ba0
rbrinson2 <rbrinson2@gmail.com>
2024-03-25 10:40:36 -0400

    Initial Commit

---
+++ Lab4_Mine_Sweep/Sources/Lab4MineSweep.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity MineSweep is
+    port(
+        clk : in std_logic;
+        rst : in std_logic
+    );
+end entity MineSweep;
+
+architecture MineSweep_ARCH of MineSweep is
+    
+begin
+    
+
+    
+end architecture MineSweep_ARCH;
