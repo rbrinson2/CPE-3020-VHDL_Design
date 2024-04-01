4254288805da30dfb35ddf52396b9c34ad9003bc
rbrinson2 <rbrinson2@gmail.com>
2024-04-01 18:44:36 -0400

    Practicum Start

---
+++ Practicum2/Sources/Practicum.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity pract is
+    port(
+        clock : in std_logic;
+        reset : in std_logic
+    );
+end entity pract;
+
+architecture pract_ARCH of pract is
+    
+begin
+    
+end architecture pract_ARCH;
