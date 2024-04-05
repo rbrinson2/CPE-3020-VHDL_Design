2e94d51c528556d3d69e8d434288c0cbbd3ac97b
rbrinson2 <rbrinson2@gmail.com>
2024-04-05 13:28:42 -0400

    Started TileDriver

---
+++ Lab4_Mine_Sweep/Sources/TileDriver.vhd
@@ -0,0 +1,18 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity TileDriver is
+    port(
+        clock : in std_logic;
+        reset : in std_logic
+    );
+end entity TileDriver;
+
+architecture TileDriver_ARCH of TileDriver is
+    
+begin
+    
+end architecture TileDriver_ARCH;
