820101c1ffccfa2c468739737418ab3acbe0607a
rbrinson2 <rbrinson2@gmail.com>
2024-04-07 12:09:33 -0400

    Move Detect Coded, starting testbench

--- Lab4_Mine_Sweep/Sources/MoveDetect.vhd
+++ Lab4_Mine_Sweep/Sources/MoveDetect.vhd
@@ -4,31 +4,71 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+--====================================================================== ENTITY
 entity MoveDetect is
     port(
+        ---------- Input Ports
         clock : in std_logic;
         reset : in std_logic;
-
         playerMoveSync : in std_logic_vector(15 downto 0);
 
+        ---------- Output Ports
         gamePlayMode : out std_logic;
         startEn : out std_logic
     );
 end entity MoveDetect;
 
+--================================================================ ARCHITECTURE
 architecture MoveDetect_ARCH of MoveDetect is
+    ---------- Constants
     constant ACTIVE : std_logic := '1';
     
+    ---------- Types
+    type detect_t is (WAITING, PLAYING, MOVEDETECTED);
+    
+    ---------- Signals
+    signal nextState    : detect_t;
+    signal currState    : detect_t;
+    signal zeroMode     : std_logic;
 begin
-   
-    DETECT : process (clock, reset) is
+    
+    --Zero-Mode-------------------------------------------- Selected Assignment
+    with playerMoveSync select
+        zeroMode <= ACTIVE when (others => '0'),
+                    not ACTIVE when others;
+    
+    --Detect-Register-------------------------------------------------- Process
+    DETECT_REG : process (clock, reset) is
     begin
-        if (reset = ACTIVE) then
-           gamePlayMode <= not ACTIVE;
-           startEn      <= not ACTIVE; 
+        if (reset = '1') then
+            currState <= WAITING;
         elsif (rising_edge(clock)) then
-            
+            currState <= nextState;
         end if;
+    end process DETECT_REG;
+    
+    --Detect----------------------------------------------------- State Machine
+    DETECT : process (currState, playerMoveSync, zeroMode) is
+    begin
+        gamePlayMode    <= not ACTIVE;
+        startEn         <= not ACTIVE;
+        case (currState) is 
+            when WAITING =>
+                if (zeroMode = ACTIVE) then
+                    nextState <= PLAYING;
+                end if;
+            when PLAYING =>
+                gamePlayMode <= ACTIVE;
+                for move in playerMoveSync'range loop
+                    if (playerMoveSync(move) = ACTIVE) then
+                        nextState <= MOVEDETECTED;
+                    end if;
+                end loop;
+            when MOVEDETECTED =>
+                startEn     <= ACTIVE;
+                nextState   <= PLAYING;
+        end case;        
+
     end process DETECT;
     
     
---
+++ Lab4_Mine_Sweep/Sources/MoveDetect_TB.vhd
@@ -0,0 +1,18 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity MoveDetect_TB is
+    port(
+        clock : in std_logic;
+        reset : in std_logic
+    );
+end entity MoveDetect_TB;
+
+
+architecture MoveDetect_TB_ARCH of MoveDetect_TB is
+    
+begin
+    
+end architecture MoveDetect_TB_ARCH;
