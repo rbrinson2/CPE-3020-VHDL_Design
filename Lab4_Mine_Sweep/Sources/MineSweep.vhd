75f44ea05d33750ea1e4fc92135b289c426d5432
rbrinson2 <rbrinson2@gmail.com>
2024-04-10 13:41:48 -0400

    State Machine is working

--- Lab4_Mine_Sweep/Sources/MineSweep.vhd
+++ Lab4_Mine_Sweep/Sources/MineSweep.vhd
@@ -36,7 +36,71 @@ end entity MineSweep;
 
 --================================================================ ARCHITECTURE
 architecture MineSweep_ARCH of MineSweep is
+    constant ACTIVE : std_logic := '1';
+    
+    type move_t is (WAITING, PLAYING, MOVEDETECTED);
+    signal currState : move_t;
+    signal nextState : move_t;
 
+    signal gamePlayMode : std_logic;
+    signal moveDet      : std_logic;
+    signal zeroMode     : std_logic;
+    
 begin
+
+    ZERO: with playerMove select
+        zeroMode <= ACTIVE when X"0000",
+                    not ACTIVE when others;
+    
+    TILEPULSE: with moveDet select
+        tiles <= X"FFFF" when ACTIVE,
+                 X"0000" when others;
+
+    
+    MOVE_REG : process (clock, reset) is
+    begin
+        if reset = ACTIVE then
+            currState <= WAITING;
+        elsif rising_edge(clock) then
+            currState <= nextState;
+        end if;
+    end process MOVE_REG;
+
+    MOVE : process(currState, playerMove, zeroMode) is
+        variable moveTraker : std_logic_vector(15 downto 0);
+    begin
+        moveDet <= '0';
+        gamePlayMode <= '0';
+
+        case currState is 
+            when WAITING =>
+                moveTraker := (others => '0');
+                if zeroMode = ACTIVE then
+                    nextState <= PLAYING;
+                else 
+                    nextState <= WAITING;
+                end if;
+            when PLAYING =>
+                gamePlayMode <= ACTIVE;
+                
+                if (playerMove = moveTraker) then
+                    nextState <= PLAYING;
+                else
+                    for move in playerMove'range loop
+                        if (playerMove(move) /= moveTraker(move)) then
+                            moveDet <= ACTIVE;
+                            moveTraker(move) := playerMove(move);
+                            nextState <= MOVEDETECTED;
+                        end if;
+                    end loop;
+                end if;
+
+            when MOVEDETECTED =>
+                gamePlayMode <= ACTIVE;
+                nextState <= PLAYING;
+        end case;
+        
+    end process MOVE;
+    
     
 end architecture MineSweep_ARCH;
