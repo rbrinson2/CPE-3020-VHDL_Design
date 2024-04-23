97ff6c6b4668038358b4d908ec4e6a38d7d5fac1
rbrinson2 <rbrinson2@gmail.com>
2024-04-23 11:59:16 -0400

    First Mask is working

--- Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
@@ -23,5 +23,39 @@ end entity ClearTiles;
 
 --Clear-Tiles===================================================== Architecture
 architecture ClearTiles_ARCH of ClearTiles is
+    signal mask1 : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    signal mask2 : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    signal mask3 : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    signal mask4 : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
 begin
+
+
+    MASK1_GEN : process (finalBombLocations) is
+        variable mask1Temp : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+        variable mask1Flag : integer range 0 to 1;
+    begin
+        for i in finalBombLocations'range loop
+            if (mask1Flag = FLAGGED) then
+                mask1Temp(i) := '0';
+            elsif (finalBombLocations(i) = '0') then
+                mask1Temp(i) := '1';
+            elsif (
+                finalBombLocations(i) = '1'
+                and mask1Flag = NOTFLAGGED
+            ) then
+                mask1Temp(i) := '0';
+                mask1Flag := FLAGGED;
+            end if;
+        end loop;
+
+        mask1 <= mask1Temp;
+    end process MASK1_GEN;
+
+    MASK2_GEN : process(finalBombLocations) is
+    begin
+        
+    end process MASK2_GEN;
+    
+    
+    
 end architecture ClearTiles_ARCH;
