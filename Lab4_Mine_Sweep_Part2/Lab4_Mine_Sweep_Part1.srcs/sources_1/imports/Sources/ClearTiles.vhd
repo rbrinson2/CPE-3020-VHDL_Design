1cfc32741f7c8d19875ac8f94784282e940446b6
rbrinson2 <rbrinson2@gmail.com>
2024-04-23 10:06:26 -0400

    Basic Test bench and file created

--- Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
@@ -4,18 +4,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.minesweeppackage.all;
+
 --Clear-Tiles=========================================================== Entity
 entity ClearTiles is
     port(
         ----------------------------------------------------------- Input Ports
-        clock           : in std_logic;
-        reset           : in std_logic
+        clock               : in std_logic;
+        reset               : in std_logic;
+        playerMoveSync      : in std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+        finalBombLocations  : in std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+        
         ---------------------------------------------------------- Output Ports
+        clearTilesMask      : out std_logic_vector(BOMBBUSWIDTH - 1 downto 0)
+
     );
 end entity ClearTiles;
 
 --Clear-Tiles===================================================== Architecture
 architecture ClearTiles_ARCH of ClearTiles is
-    
 begin
 end architecture ClearTiles_ARCH;
--- Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles_TB.vhd
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles_TB.vhd
@@ -3,15 +3,64 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.minesweeppackage.all;
+
+--====================================================================== ENTITY
 entity ClearTiles_TB is
-    port(
-        clock : in std_logic;
-        reset : in std_logic
-    );
 end entity ClearTiles_TB;
 
+--================================================================ ARCHITECTURE
 architecture ClearTiles_TB_ARCH of ClearTiles_TB is
+    type StimArray_t is array (natural range <>) of std_logic_vector(BOMBBUSWIDTH - 1 downto 0);   
+    constant stim : StimArray_t := (
+        ---------PlayerMoveSync----------
+        "0000" & "0000" & "0000" & "0000",
+        "0000" & "0000" & "0000" & "0001"
+    );
     
+
+    signal clock               : std_logic := '0';
+    signal reset               : std_logic := '1'; 
+    signal playerMoveSync      : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    signal finalBombLocations  : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    signal clearTilesMask      : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+
 begin
+
+    clock <= not clock after 10 ns;
+    reset <= not ACTIVE after 40 ns;
+    finalBombLocations <= "0001" & "1000" & "1100" & "0010" when reset = not ACTIVE else X"0000";
     
+    DUT : entity work.ClearTiles
+        port map(
+            clock              => clock,
+            reset              => reset,
+            playerMoveSync     => playerMoveSync,
+            finalBombLocations => finalBombLocations,
+            clearTilesMask     => clearTilesMask
+        );
+    
+    STIMULUS : process (clock, reset) is
+        variable count : integer := 0;
+        variable i : integer := 0;
+        
+    begin
+        if (reset = ACTIVE) then
+            playerMoveSync <= stim(0);
+            count := 0;
+            i := 0;
+        elsif rising_edge(clock) then
+            count := count + 1;
+            if (count mod 10 = 5) then
+                if (i < stim'length) then
+                    playerMoveSync <= stim(i);
+                    i := i + 1;
+                else 
+                    std.env.stop;
+                end if;
+            end if;
+        end if;
+    end process STIMULUS;
+    
+
 end architecture ClearTiles_TB_ARCH;
