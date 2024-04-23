0de15e52f619e9eb9315ca6d0732fd80340cfbe4
rbrinson2 <rbrinson2@gmail.com>
2024-04-23 15:55:27 -0400

    Got some logic working in sim

--- Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles.vhd
@@ -16,15 +16,62 @@ entity ClearTiles is
         finalBombLocations  : in std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
         
         ---------------------------------------------------------- Output Ports
-        clearTilesMask      : out std_logic_vector(BOMBBUSWIDTH - 1 downto 0)
+        clearTilesMask      : out std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+        hitDet              : out std_logic
 
     );
 end entity ClearTiles;
 
 --Clear-Tiles===================================================== Architecture
 architecture ClearTiles_ARCH of ClearTiles is
-    
+    signal newMove : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
 begin
+    PLAYER_EDGE : for i in playerMoveSync'range generate
+        LEVEL_DETECT : entity work.LevelDetector
+            port map(
+                reset    => reset,
+                clock    => clock,
+                trigger  => playerMoveSync(i),
+                pulseOut => newMove(i)
+            );
+    end generate PLAYER_EDGE;
+    
+    HIT_SCAN : process (clock, reset) is
+        variable clearTemp : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+        variable j : integer range 0 to 16;
+    begin
+        if (reset = ACTIVE) then
+            hitDet <= not ACTIVE;
+            clearTemp := (others => '0'); 
+            j := 0;
+        elsif rising_edge(clock) then
+            for i in finalBombLocations'reverse_range loop
+                if (finalBombLocations(i) = '1' and newMove(i) = '1') then
+                    hitDet <= ACTIVE;
+                elsif (newMove(i) = '1') then
+                    j := i;
+                    while (finalBombLocations(j) /= '1') loop
+                        clearTemp(j) := '1';
+                        if (j = 15) then
+                            exit;
+                        end if;
+                        j := j + 1;
+                    end loop;
+                    j := i;
+                    while (finalBombLocations(j) /= '1') loop
+                        clearTemp(j) := '1';
+                        if (j = 0) then
+                            exit;
+                        end if;
+                        j := j - 1;
+                    end loop;
+                end if;
+            end loop;
+        end if;
+
+        clearTilesMask <= clearTemp;
+    end process HIT_SCAN;
+    
     
     
 end architecture ClearTiles_ARCH;
