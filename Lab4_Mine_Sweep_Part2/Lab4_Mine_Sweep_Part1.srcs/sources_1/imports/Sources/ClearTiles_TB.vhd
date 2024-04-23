392dbc6fa9cdf6b4485bc8f2f198aafb2aa24413
rbrinson2 <rbrinson2@gmail.com>
2024-04-23 09:00:04 -0400

    Starting Clear Tiles over again

---
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/ClearTiles_TB.vhd
@@ -0,0 +1,17 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ClearTiles_TB is
+    port(
+        clock : in std_logic;
+        reset : in std_logic
+    );
+end entity ClearTiles_TB;
+
+architecture ClearTiles_TB_ARCH of ClearTiles_TB is
+    
+begin
+    
+end architecture ClearTiles_TB_ARCH;
