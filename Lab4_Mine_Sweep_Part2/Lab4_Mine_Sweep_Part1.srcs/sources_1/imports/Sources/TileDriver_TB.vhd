1086c3e41226abce1adb56a404cc3e415b7a835d
rbrinson2 <rbrinson2@gmail.com>
2024-04-24 15:50:21 -0400

    Preliminary flashing working in sim

--- Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/TileDriver_TB.vhd
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/TileDriver_TB.vhd
@@ -26,18 +26,41 @@ end entity TileDriver_TB;
 
 --================================================================ ARCHITECTURE
 architecture TileDriver_TB_ARCH of TileDriver_TB is
+
+    type StimStruct_t is record
+        hit : std_logic;
+        move : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
+    end record StimStruct_t;
+    
+    type StimArray_t is array (natural range <>) of StimStruct_t;   
+    constant stim : StimArray_t := (
+        ---Hits|--------PlayerMoveSync----------
+        -------|"0001" & "1000" & "1100" & "0010"--Bomb-Location
+          ('0', "0000" & "0000" & "0000" & "0000"),
+          ('0', "1110" & "0000" & "0000" & "0000"),
+          ('0', "1110" & "0111" & "0000" & "0000"),
+          ('0', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001"),
+          ('1', "1110" & "0111" & "0000" & "0001")
+    );
     
     signal clock            : std_logic := '0';
     signal reset            : std_logic;
     signal bombLocation     : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
     signal tiles            : std_logic_vector(TILEBUSWIDTH - 1 downto 0);
     signal hitDet           : std_logic;
+    signal doneMode         : std_logic;
     signal clearTilesMask   : std_logic_vector(BOMBBUSWIDTH - 1 downto 0);
     
 begin
 
     clock <= not clock after 10 ns;
     reset <= '1', '0' after 40 ns;    
+    bombLocation <= "0001" & "1000" & "1100" & "0010" when reset = not ACTIVE else X"0000";
 
     --Tile-Driver------------------------------------------------------ Instant
     DUT : entity work.TileDriver
@@ -47,31 +70,30 @@ begin
             bombLocation   => bombLocation,
             clearTilesMask => clearTilesMask,
             hitDet         => hitDet,
+            doneMode       => doneMode,
             tiles          => tiles
         );
     
     --Stimulus--------------------------------------------------------- Process
-    STIMULUS : process (clock) is
-        variable seed   : integer := 1;
+    STIMULUS : process (clock, reset) is
+        variable index  : integer := 0;
         variable count  : integer := 0;
-        variable x      : real;
-        variable rand   : integer;
-        
     begin
-
-        if (rising_edge(clock)) then
-            -- Generate random rumber
-            uniform(seed, seed, x);
-            rand := integer(floor(x * 32768.0));
-            report "rand = " & integer'image(rand);
-            
-            -- Run 10 different random numbers
+        if (reset = ACTIVE) then
+            count := 0;
+            index := 0;
+            clearTilesMask <= stim(0).move;
+            hitDet <= stim(0).hit;
+        elsif (rising_edge(clock)) then
             count := count + 1;
-            if (count mod 5 = 0) then
-                -- Update bomblocations with the random number
-                bombLocation <= std_logic_vector(to_unsigned(rand, 16));
-            elsif (count >= 100) then
-                std.env.stop;
+            if (count mod 10 = 5) then
+                clearTilesMask <= stim(index).move;
+                hitDet <= stim(index).hit;
+                if (index < stim'length - 1) then
+                    index := index + 1;
+                elsif (count >= 150) then
+                    std.env.stop;
+                end if;
             end if;
         end if;
     end process STIMULUS;
