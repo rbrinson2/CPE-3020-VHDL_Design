d331c88ee8f699f7ab9294f1656a706fdbd9d728
rbrinson2 <rbrinson2@gmail.com>
2024-04-25 09:06:38 -0400

    Initial Commit

---
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/Timer.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Timer is
+    port(
+        clock : in std_logic;
+        reset : in std_logic
+    );
+end entity Timer;
+
+
+architecture Timer_ARCH of Timer is
+    
+begin
+    
+end architecture Timer_ARCH;
---
+++ Lab4_Mine_Sweep_Part2/Lab4_Mine_Sweep_Part1.srcs/sources_1/imports/Sources/Timer_TB.vhd
@@ -0,0 +1,15 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Timer_TB is
+end entity Timer_TB;
+
+
+architecture Timer_TB_ARCH of Timer_TB is
+    
+begin
+    
+end architecture Timer_TB_ARCH;
